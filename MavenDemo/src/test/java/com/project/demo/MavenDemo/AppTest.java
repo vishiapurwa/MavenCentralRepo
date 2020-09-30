211bf32f89aa9cffe7ef44198c5d31f024967eae
vishiapurwa <vishiapurwa@gmail.com>
2020-09-30 12:10:21 +0530

    Added AppTest Class

--- MavenDemo/src/test/java/com/project/demo/MavenDemo/AppTest.java
+++ MavenDemo/src/test/java/com/project/demo/MavenDemo/AppTest.java
@@ -1,38 +1,27 @@
 package com.project.demo.MavenDemo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.fail;
 
-/**
- * Unit test for simple App.
- */
+import org.junit.Test;
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void test1() {
+		System.out.println("In test1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("In test2");
+	}
+	@Test
+	public void test3() {
+		fail();
+	}
+	@Test
+	public void test4() {
+		fail();
+	}
+    
 }
